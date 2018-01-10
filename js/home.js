6cd8de5c643c1edf280adb73cbd973db66c65761
gabrielamf <glennygabb@gmail.com>
2018-01-10 00:54:41 -0500

    añade funcion filtrado por tipo

--- js/home.js
+++ js/home.js
@@ -15,34 +15,60 @@ $(document).ready(function() {
     }
   });
 
-
-});
-
   // button write
   var elem = $('.fixed-action-btn');
   $('.modal').modal();
-
+  
   // funcion para filtrado
   var container = $('#container-posts');
   for (i = 0; i < data.length; i++) {
     var grupo = 
-    '<div class="col s6 collection">' +
-      '<img class="responsive-img modal-trigger" href="#modal-post" src=' + data[i].image + '>' +
-      '<p class="">' + data[i].name + '</p>' +
-    '</div>';
+  '<div class="col s6 collection card-image card-border" data-type=' + data[i].type + ' >' + 
+    '<div class="card">' +
+      '<div class="card-image">' +
+      '<img id="post-height" class="responsive-img modal-trigger content" data-name =' + data[i].name + ' data-image =' + data[i].image + ' data-user =' + data[i].user + ' data-description = "' + data[i].description + '" href="#modal-post" src=' + data[i].image + '>' +
+    '</div>' +
+    '<div class="card-content">' +
+    '<p>' + data[i].name + '</p>' +
+    '</div>' +
+  '</div>';
     container.append(grupo);
   }
-	
-  // filtro
-  $('#search').keyup(function(event) {
+
+  $('.heart').click(function(event) {
     console.log(event.target);
-    var name = $(this).val();
+    $(this).toggleClass('favorite');
+  });
+    
+  // FILTRO DE POSTS
+  $('#search-post').keyup(function() {
+    var name = $(this).val().toLowerCase();
     $('.collection').hide();
     $('.collection').each(function() {
-      var search = $(this).text().toLowerCase();
-      if (search.indexOf(name) !== -1) {
+      var search = $(this).text();
+      if (search.indexOf(name) !== -1) { // filtrado por nombre
         $(this).show();
       }
     });
+    $('.collection').each(function() { 
+      var search = $(this).text();
+      var type = $(this).data('type');// filtrado por tipo
+      if (type.indexOf(name) !== -1) {
+        $(this).show();
+      }
+    });
+  });
+
+  // MODAL
+  $('.content').click(function() {
+    var name = $(this).data('name'),
+      image = $(this).data('image'),
+      description = $(this).data('description'),
+      user = $(this).data('user');
+      
+    $('.name').text(name);
+    $('.image').attr('src', image);
+    $('.description').text(description);
+    $('.user-post').attr('src', user);
   });
 });
